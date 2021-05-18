c602af36d121820b85bc00ca74d026c9d7503b54
Leandro <leandro@leandroleite.info>
2021-05-18 18:14:19 -0300

    chore: CI build stage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,8 @@
 #! /bin/bash
 
 if [ "$APP_ENV" = 'production' ]; then
-
-  if [ ! -d "/app/dist" ]; then
-    yarn build
-  fi
-
   yarn start
-fi
-
-if [ "$APP_ENV" = 'local' ]; then
+elif [ "$APP_ENV" = 'local' ]; then
   yarn dev
 fi
 
